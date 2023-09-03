7014811a5cb00977340c3c72cb832cf135abe25f
FernandoGupiocRios <142682746+FernandoGupiocRios@users.noreply.github.com>
2023-09-02 23:50:09 -0500

    Add files via upload
    ejercicios  parte 2 texto escrito

---
+++ ejercicio03.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package evaluacionpoo01;
+
+/**
+ *
+ * @author fernandogupioc
+ */
+public class ejercicio03 {
+      public static void main(String[] args) {
+   int numA = 5; 
+int numB = 10; 
+
+// Verifica si tanto numA como numB son positivos
+boolean sonPositivos = numA > 0 && numB > 0;
+
+// Imprime el resultado
+System.out.println(sonPositivos);
+
+          
+          
+          
+      }
+}
---
+++ ejercicio04.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package evaluacionpoo01;
+
+/**
+ *
+ * @author fernandogupioc
+ */
+public class ejercicio04 {
+      public static void main(String[] args) {
+int numA = -5; 
+int numB = 10; 
+boolean mismoSigno = (numA >= 0 && numB >= 0) || (numA < 0 && numB < 0);
+
+System.out.println(mismoSigno);
+
+          
+          
+          
+      }
+}
