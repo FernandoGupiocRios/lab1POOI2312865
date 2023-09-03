4d810fc10fb664f7472eca8d322435288f81125d
FernandoGupiocRios <142682746+FernandoGupiocRios@users.noreply.github.com>
2023-09-02 23:43:57 -0500

    Add files via upload
    Ejercicios parte 2 la parte escrita.

---
+++ EvaluacionPooparte2/EvaluacionPoo01.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package evaluacionpoo01;
+
+/**
+ *
+ * @author fernandogupioc
+ */
+public class EvaluacionPoo01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int num = 7; // puedes cambiar el valor de num según tus necesidades
+
+// Verifica si num es impar y positivo
+boolean esImparYPositivo = num % 2 != 0 && num > 0;
+
+// Imprime el resultado
+System.out.println(esImparYPositivo);
+
+    }
+    
+}
