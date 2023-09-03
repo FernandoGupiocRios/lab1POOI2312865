3e3295eccfcea84fa49cb2f904c2ec1b999fa597
FernandoGupiocRios <142682746+FernandoGupiocRios@users.noreply.github.com>
2023-09-02 23:29:57 -0500

    EJERCICIOSPOO01

---
+++ Ejercicio008.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyectooficialpoo;
+
+/**
+ *
+ * @author fernandogupioc
+ */
+public class Ejercicio008 {
+    
+  
+  public static void main(String[] args) { 
+
+int ×=1, y=2, z=3;
+
+if (x<y){
+System.out.println("####");
+System.out.println("****");
+}
+
+else
+System.out.println("§§d&");
+//se imprimirá "####" y "****" en la salida.
+    }
+}
---
+++ Ejercicio009.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyectooficialpoo;
+
+/**
+ *
+ * @author fernandogupioc
+ */
+public class Ejercicio009 {
+    
+    
+       public static void main(String[] args) { 
+
+           
+int ×=1, y=2, z=3;
+if ('×' > 'y' || 66 > (int)('A'))
+System.out .println("#*#");
+           
+           //da como resultado #*#
+
+       }
+}
